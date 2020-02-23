2667e249b32150ba416afd8daa2dbc6228475577
Nick Krause <nick@codeparticle.com>
2020-02-22 17:15:59 -0800

    feat(selectors): simplify selectors generation

--- src/core/generate-selectors.ts
+++ src/core/generate-selectors.ts
@@ -3,21 +3,17 @@ import { formatSelectorName } from '../internal/string-helpers/formatters'
 import { get } from '../utils/get'
 import { getObjectPaths } from '../utils/get-object-paths'
 
-const generateSelectors = <T = object>(initialState: T): SelectorsObject => {
+const generateSelectors = <T>(initialState: T, prefix = ``): SelectorsObject<T> => {
   const selectorPaths = getObjectPaths(initialState)
-  const selectors = {}
 
-  for (let i = selectorPaths.length - 1; i > -1; i--) {
-    const formatted = selectorPaths[i].join(`_`)
+  return selectorPaths.reduce((acc, path) => {
+    const formatted = [].concat(path).join(`_`)
 
-    selectors[formatSelectorName(formatted)] = newState => get(
-      newState,
-      selectorPaths[i],
-      get(initialState, selectorPaths[i]),
-    )
-  }
+    acc[formatSelectorName(formatted, prefix)] = newState => get(newState, path, get(initialState, path))
 
-  return selectors
+    return acc
+  }, {})
+
 }
 
 export { generateSelectors }
