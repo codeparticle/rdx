76204f1aca4ba33fb5b6b0f80feb021cfe403f13
Nick Krause <nick@codeparticle.com>
2020-02-22 17:40:22 -0800

    feat(actions): make actions aware of prefixes

--- src/core/generate-actions.ts
+++ src/core/generate-actions.ts
@@ -2,7 +2,7 @@ import { createAction } from './create-action'
 import { RdxDefinition, ActionObject, TypesObject } from '../types'
 import { formatActionName, formatTypeString } from '../internal'
 
-const generateActions: (types: TypesObject) => ActionObject = types => {
+const generateActions: (types: TypesObject) => ActionObject<any> = types => {
   return Object.keys(types).reduce((actions, typeName) => {
     actions[formatActionName(typeName, ``, { reset: typeName.includes(`RESET`) })] = createAction(
       typeName,
@@ -12,19 +12,20 @@ const generateActions: (types: TypesObject) => ActionObject = types => {
   }, {})
 }
 
-const generateActionsFromDefs: (defs: RdxDefinition[]) => ActionObject = (defs = []) => {
+const generateActionsFromDefs: (defs: RdxDefinition[], prefix?: string) => ActionObject<any> = (defs = [], prefix = ``) => {
   const actions = {}
   let idx = defs.length
 
   while(idx--) {
     const { reducerName, definitions } = defs[idx]
 
-    actions[formatActionName(reducerName)] = createAction(formatTypeString(reducerName))
-    actions[formatActionName(reducerName, ``, { reset: true })] = createAction(
-      formatTypeString(reducerName, ``, { reset: true }),
+    actions[formatActionName(reducerName, prefix)] = createAction(formatTypeString(reducerName, prefix))
+    actions[formatActionName(reducerName, prefix, { reset: true })] = createAction(
+      formatTypeString(reducerName, prefix, { reset: true }),
     )
+
     definitions.map(({ actionName, typeName }) => {
-      actions[actionName] = createAction(typeName)
+      actions[formatActionName(actionName, prefix)] = createAction(formatTypeString(typeName, prefix))
     })
   }
 
