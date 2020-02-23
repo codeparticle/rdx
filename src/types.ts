3980dee9933f299f0e8c3824d0ef613672abaf8c
Nick Krause <nick@codeparticle.com>
2020-02-22 17:42:48 -0800

    feat(tests): update tests, types

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,30 @@
-export type TypesObject<T = { [key: string]: string }> = Record<keyof T, keyof T>;
-export type StateValue = any;
-export type StateObject = { [key: string]: StateValue };
+import { Store, Middleware } from "redux"
+import { EnhancerOptions } from "redux-devtools-extension"
+import { SagaMiddlewareOptions } from "redux-saga"
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartial<U>>
+    : DeepPartial<T[P]>;
+}
+
+export type TypesObject<T = { [key: string]: string }> = Record<keyof T, keyof T>
+export type StateValue = any
+export type StateObject = { [key: string]: StateValue }
 
 export type Action<T = never> = {
   type: string
   payload?: T
   id: string
-};
+}
 
-export type ActionObject<T = any> = Record<string, ActionCreator<T>>;
+export type ActionObject<State=any> = Record<string, ActionCreator<DeepPartial<State> | any>>
 
-export type ActionCreator<T = undefined> = (payload?: T, id?: string) => Action<T> | Action<never>;
+export type DerivedActionObject<State> = Record<keyof RdxSubmodule<State>['actions'], ActionCreator<DeepPartial<State>>>
+
+export type ActionCreator<T = any> = (payload?: T, additionalKeys?: object, id?: string) => Action<T> | Action<never>
 
 export type TypeDef = {
   typeName: string
@@ -20,69 +34,74 @@ export type TypeDef = {
   handlerType: "string" | "number" | "boolean" | "array" | "object" | "default"
   initialState: any
   raw?: string
-};
+}
 
 export type ConditionalFilter<T = Array<string>> = (
   item: string,
   index: number,
   collection: T
-) => boolean;
+) => boolean
 
 export type UserDefinedReducers = {
   [key: string]: StateObject
-};
+}
 
-export type SelectorsObject = Record<string, (state: object) => any>;
+export type SelectorFunction<State, Backup=DeepPartial<State>> = (state: State) => DeepPartial<State> | Backup
 
-export type Reducer<S, A = Action<S>> = (state: S, action: A) => S;
+export type SelectorsObject<State> = Record<string, SelectorFunction<State | any>>
 
-export type RdxDefinition = { reducerName?: string; definitions: Array<TypeDef> };
+export type Reducer<S, A = Action<S>> = (state: S, action: A) => S
+
+export type RdxDefinition = { reducerName?: string; definitions: Array<TypeDef> }
 
 export type RdxConfiguration = {
   prefix?: string
   sagas?: Record<string, Generator>
-
-};
+}
 
 export type RdxOutput<State> = {
-  actions: ActionObject
+  actions: ActionObject<State>
   reducers: Reducer<State>
-  selectors: SelectorsObject
+  selectors: SelectorsObject<State>
   types: TypesObject
   sagas?: { [key: string]: Generator }
-};
+  prefix: string
+  state: State
+}
 
 export type HandlerConfig<State> = {
-  handlerType: TypeDef["handlerType"]
-  reducerKey: string
-  partial: boolean
+  handlerType?: TypeDef["handlerType"]
+  reducerKey?: string
+  partial?: boolean
   reset: boolean
   initialState: State
-};
+}
 
 export type Handler<State> =
   | ((initialState: State) => Reducer<State>)
   | ((key: string) => Reducer<State>)
-  | Reducer<State>;
+  | Reducer<State>
 
 export type PregeneratedReducerKeys<State = any> = {
   key: string
   handlers: Record<string, Handler<State>>
   handlerType: string
   initialState: State
-};
+}
 
 export type PregeneratedReducer<State = any> = {
   name: string
   keys: PregeneratedReducerKeys<State>[]
-};
+  reducerState: State
+  reducerHandlers: Record<string, Handler<State>>
+}
 
 export type ApiRequestState = {
   loaded: boolean
   failed: boolean
   error: any
   data: any
-};
+}
 
 export type ConfiguredSagasObject = {
   latest?: DefaultSagasObject
@@ -92,7 +111,67 @@ export type ConfiguredSagasObject = {
 type NotLatestOrEvery = Exclude<string, keyof ConfiguredSagasObject>
 
 export type DefaultSagasObject = {
-    [key in NotLatestOrEvery]: <ActionType=any>(action?: Action<ActionType>) => Generator
+  [key in NotLatestOrEvery]: <ActionType = any>(action?: Action<ActionType>) => Generator;
 }
 
 export type SagasObject = ConfiguredSagasObject | DefaultSagasObject
+
+export type RdxSagasConfig = {
+  enabled: boolean
+  options?: SagaMiddlewareOptions
+}
+
+export interface CombinedModule<CombinedTypes, CombinedState, CombinedActions, CombinedSelectors> {
+  types: TypesObject<CombinedTypes>
+  actions: CombinedActions
+  selectors: CombinedSelectors
+  reducers: Reducer<CombinedState>
+  state: CombinedState
+}
+
+export type RdxSubmodule<State> = ReturnType<() => RdxOutput<State>>
+
+export type ModuleCombination<State> = CombinedModule<
+  RdxSubmodule<State>["types"],
+  RdxSubmodule<State>["state"],
+  RdxSubmodule<State>["actions"],
+  RdxSubmodule<State>["selectors"]
+> & RdxMappers<RdxSubmodule<State>["actions"], RdxSubmodule<State>["selectors"]>
+
+export type RdxRootConfiguration<State = StateObject> = {
+  modules: ModuleCombination<State>
+  config?: {
+    middleware?: Array<Middleware>
+    devtools?: RdxDevToolsConfig
+    sagas?: RdxSagasConfig
+    provideMappers?: boolean
+  }
+}
+
+export type ActionMapper<A> = <Actions=A>(
+  actions: Actions
+)
+=> <B=Actions, Aliases=Record<string, keyof B>>(...vs: (keyof B)[] | Aliases[])
+=> <C=B, Names=Aliases & Partial<C>>(dispatch: any)
+=> Record<keyof Names, ActionCreator>
+
+export type SelectionMapper<S> = <Selectors = S>(
+  selectors: Selectors
+) => <A = Selectors>(
+  ...vs: (keyof A)[] | Record<string, keyof A>[]
+) => <State>(state: State) => Record<string, ReturnType<SelectorFunction<State>>>
+
+export type ConfiguredStore<State> = ModuleCombination<State> & {
+  store: Store<State>
+  runSagas?: any
+}
+
+export type RdxMappers<A, S> = {
+  mapActions?: ReturnType<ActionMapper<A>>
+  mapState?: ReturnType<SelectionMapper<S>>
+}
+
+export type RdxDevToolsConfig = {
+  enabled: boolean
+  options?: EnhancerOptions
+}
--- test/rdx.test.ts
+++ test/rdx.test.ts
@@ -2,27 +2,29 @@
  * @file Unit tests for @codeparticle/rdx
  */
 
-import { createStore, applyMiddleware } from 'redux'
 import { createNames } from '../src/internal'
-import { generateActions, generateActionsFromDefs } from '../src/core/generate-actions'
-import { defineState } from '../src/core/generate-defs'
-import { createAction } from '../src/core/create-action'
-import { generateReducersFromDefs } from '../src/core/generate-reducers'
-import { generateSelectors } from '../src/core/generate-selectors'
-import { generateTypesFromDefs, generateTypes, prefixTypes } from '../src/core/generate-types'
-import { rdx, apiState } from '../src/rdx'
-import createSagaMiddleware from 'redux-saga'
-import { put } from 'redux-saga/effects'
+import {
+  defineState,
+  createAction,
+  generateReducersFromDefs,
+  generateSelectors,
+  generateTypes,
+  generateMappers,
+  combineModules,
+  createStore,
+  rdx,
+  apiState,
+} from "../src/rdx"
+
+import * as utils from '../src/utils'
+import { put, select } from 'redux-saga/effects'
 import { combineSagas, generateSagas } from '../src/sagas'
 
 describe(`RDX`, () => {
-  const prefix = `prefix`
 
-  const objectUserDefs = {
-    app: {
-      lightSwitch: true,
-      metadata: { isCool: false },
-    },
+  const module1State = {
+    lightSwitch: true,
+    metadata: { isCool: true },
     mega: {
       num: 2,
       string: ``,
@@ -31,205 +33,381 @@ describe(`RDX`, () => {
       todos: [],
     },
     apiCall: apiState,
+    deeply: {
+      nested: {
+        object: {
+          is: {
+            fine: true, // but only for selectors
+          },
+        },
+      },
+    },
+  }
+  const module2State = {
+    wow: `big if true`,
   }
 
-  const reduxModule = rdx(objectUserDefs, { prefix })
-  const selectors = generateSelectors(objectUserDefs)
-  const defs = defineState(objectUserDefs)
+  const module1 = rdx({ prefix: `app` })(module1State)
+  const module2 = rdx({ prefix: `whoa` })(module2State)
+  const modules = combineModules<{ app: typeof module1['state']; whoa: typeof module2['state']}>(
+    module1,
+    module2,
+  )
+
+  type CombinedModules = typeof modules['actions']
+
+  const defs = defineState(module1State)
   const customTypes = generateTypes`
     WOW
     COOL_DUDE
     SWEET
   `
-  const types = generateTypesFromDefs(defs)
-  const prefixedTypes = prefixTypes(prefix)(types)
-  const actions = generateActions(types)
 
-  const expectedActions = {
-    setApp: expect.any(Function),
-    resetApp: expect.any(Function),
+  const storeActions = {
+    resetAppApiCall: expect.any(Function),
+    resetAppMega: expect.any(Function),
+    resetAppTodo: expect.any(Function),
+    resetAppDeeply: expect.any(Function),
+    resetAppLightSwitch: expect.any(Function),
+    resetAppMetadata: expect.any(Function),
+    resetWhoaWow: expect.any(Function),
+    setAppApiCall: expect.any(Function),
+    setAppApiCallData: expect.any(Function),
+    setAppApiCallError: expect.any(Function),
+    setAppApiCallFailed: expect.any(Function),
+    setAppApiCallFetching: expect.any(Function),
+    setAppApiCallLoaded: expect.any(Function),
     setAppLightSwitch: expect.any(Function),
+    setAppMega: expect.any(Function),
+    setAppMegaNum: expect.any(Function),
+    setAppMegaString: expect.any(Function),
     setAppMetadata: expect.any(Function),
-    setTodo: expect.any(Function),
-    resetTodo: expect.any(Function),
-    setTodoTodos: expect.any(Function),
-    setMega: expect.any(Function),
-    resetMega: expect.any(Function),
-    setMegaString: expect.any(Function),
-    setMegaNum: expect.any(Function),
-    setApiCall: expect.any(Function),
-    setApiCallLoaded: expect.any(Function),
-    setApiCallFailed: expect.any(Function),
-    setApiCallError: expect.any(Function),
-    setApiCallData: expect.any(Function),
-    setApiCallFetching: expect.any(Function),
-    resetApiCall: expect.any(Function),
+    setAppMetadataIsCool: expect.any(Function),
+    setAppTodo: expect.any(Function),
+    setAppTodoTodos: expect.any(Function),
+    setWhoaWow: expect.any(Function),
+    setAppDeeply: expect.any(Function),
+    setAppDeeplyNested: expect.any(Function),
   }
 
-  const expectedSelectors = {
+  const storeSelectors = {
     getApp: expect.any(Function),
+    getAppApiCall: expect.any(Function),
+    getAppApiCallData: expect.any(Function),
+    getAppApiCallError: expect.any(Function),
+    getAppApiCallFailed: expect.any(Function),
+    getAppApiCallFetching: expect.any(Function),
+    getAppApiCallLoaded: expect.any(Function),
     getAppLightSwitch: expect.any(Function),
+    getAppMega: expect.any(Function),
+    getAppMegaNum: expect.any(Function),
+    getAppMegaString: expect.any(Function),
     getAppMetadata: expect.any(Function),
-    getTodo: expect.any(Function),
-    getTodoTodos: expect.any(Function),
-    getMega: expect.any(Function),
-    getMegaString: expect.any(Function),
-    getMegaNum: expect.any(Function),
-    getApiCall: expect.any(Function),
-    getApiCallLoaded: expect.any(Function),
-    getApiCallFailed: expect.any(Function),
-    getApiCallError: expect.any(Function),
-    getApiCallData: expect.any(Function),
-    getApiCallFetching: expect.any(Function),
+    getAppMetadataIsCool: expect.any(Function),
+    getAppDeeply: expect.any(Function),
+    getAppDeeplyNested: expect.any(Function),
+    getAppDeeplyNestedObject: expect.any(Function),
+    getAppDeeplyNestedObjectIs: expect.any(Function),
+    getAppDeeplyNestedObjectIsFine: expect.any(Function),
+    getAppTodo: expect.any(Function),
+    getAppTodoTodos: expect.any(Function),
+    getWhoa: expect.any(Function),
+    getWhoaWow: expect.any(Function),
   }
 
-  const expectedReducers = {
-    app: expect.any(Function),
-    todo: expect.any(Function),
-    mega: expect.any(Function),
-    apiCall: expect.any(Function),
+  const storeTypes = {
+    SET_APP_API_CALL: `SET_APP_API_CALL`,
+    SET_APP_API_CALL_DATA: `SET_APP_API_CALL_DATA`,
+    SET_APP_API_CALL_ERROR: `SET_APP_API_CALL_ERROR`,
+    SET_APP_API_CALL_FAILED: `SET_APP_API_CALL_FAILED`,
+    SET_APP_API_CALL_FETCHING: `SET_APP_API_CALL_FETCHING`,
+    SET_APP_API_CALL_LOADED: `SET_APP_API_CALL_LOADED`,
+    SET_APP_LIGHT_SWITCH: `SET_APP_LIGHT_SWITCH`,
+    SET_APP_MEGA: `SET_APP_MEGA`,
+    SET_APP_MEGA_NUM: `SET_APP_MEGA_NUM`,
+    SET_APP_MEGA_STRING: `SET_APP_MEGA_STRING`,
+    SET_APP_METADATA: `SET_APP_METADATA`,
+    SET_APP_TODO: `SET_APP_TODO`,
+    SET_APP_TODO_TODOS: `SET_APP_TODO_TODOS`,
+    SET_WHOA_WOW: `SET_WHOA_WOW`,
+    SET_APP_DEEPLY: `SET_APP_DEEPLY`,
+    SET_APP_DEEPLY_NESTED: `SET_APP_DEEPLY_NESTED`,
+    SET_APP_METADATA_IS_COOL: `SET_APP_METADATA_IS_COOL`,
   }
 
-  it(`should be able to generate reducer, type, action, and selector names from an initial state object`, () => {
-    const expectedNames = {
-      typeName: `SET_ANY_STRING`,
-      actionName: `setAnyString`,
-      selectorName: `getAnyString`,
-      reducerKey: `anyString`,
-    }
-    const actualNames = createNames(`anyString`)
-
-    for (const name of Object.values(actualNames)) {
-      expect(Object.values(expectedNames).includes(name)).toBeTruthy()
-    }
-  })
-  it(`should create actions from a template string with createTypes`, () => {
-    const customTypesObjectKeys = [
-      `WOW`,
-      `COOL_DUDE`,
-      `SWEET`,
-    ]
-
-    for (const val of customTypesObjectKeys) {
-      expect(Object.keys(customTypes).includes(val as string)).toBeTruthy()
-    }
-
-    expect(types).toEqual({
-      SET_APP: `SET_APP`,
-      RESET_APP: `RESET_APP`,
-      SET_APP_LIGHT_SWITCH: `SET_APP_LIGHT_SWITCH`,
-      SET_APP_METADATA: `SET_APP_METADATA`,
-      SET_MEGA: `SET_MEGA`,
-      RESET_MEGA: `RESET_MEGA`,
-      SET_MEGA_NUM: `SET_MEGA_NUM`,
-      SET_MEGA_STRING: `SET_MEGA_STRING`,
-      SET_TODO: `SET_TODO`,
-      RESET_TODO: `RESET_TODO`,
-      SET_TODO_TODOS: `SET_TODO_TODOS`,
-      RESET_API_CALL: `RESET_API_CALL`,
-      SET_API_CALL: `SET_API_CALL`,
-      SET_API_CALL_DATA: `SET_API_CALL_DATA`,
-      SET_API_CALL_ERROR: `SET_API_CALL_ERROR`,
-      SET_API_CALL_FETCHING: `SET_API_CALL_FETCHING`,
-      SET_API_CALL_LOADED: `SET_API_CALL_LOADED`,
-      SET_API_CALL_FAILED: `SET_API_CALL_FAILED`,
+  describe(`createStore`, () => {
+    const { types, reducers, actions, selectors, store, runSagas } = createStore({
+      modules,
     })
-  })
 
-  it(`should accept a curried version of createAction`, () => {
-    const expectedAction = {
-      type: `type`,
-      payload: true,
-      id: `type`,
-    }
-
-    expect(createAction(`type`)(true)).toEqual(expectedAction)
-  })
-
-  it(`should generate actions with generateActions`, () => {
-    expect(actions).toMatchObject(expectedActions)
-  })
-
-  it(`should generate actions from a set of reducer definitions`, () => {
-    expect(generateActionsFromDefs(defs)).toMatchObject(expectedActions)
-  })
-
-  it(`should generate reducers from a set of reducer definitions`, () => {
-    expect(generateReducersFromDefs(defs)).toMatchObject(expectedReducers)
-  })
-
-  it(`should generate selectors from an initial state object`, () => {
-    expect(selectors).toMatchObject(expectedSelectors)
-
-    expect(selectors[`getMegaNum`]({ ...objectUserDefs, mega: { num: undefined } })).toBe(2)
-  })
-
-  it(`should generate a redux module from a set of reducer definitions`, () => {
-    expect(reduxModule).toMatchObject({
-      reducers: expect.any(Function),
-      types: prefixedTypes,
-      actions: expectedActions,
-      selectors: expectedSelectors,
-    })
-  })
-
-  it(`actually works with redux`, () => {
-    const { actions, selectors, reducers } = reduxModule
     const sagaActionType = `sagaActionType`
+    const sagaAction = createAction(sagaActionType)
 
     const sagas = generateSagas({
       every: {
         [sagaActionType]: function*() {
-          yield put(actions.setApiCallData({ sagaWorkedOnEvery: true }))
+          yield put(actions.setAppApiCallData({ sagaWorkedOnEvery: true }))
+          const apiCall = yield select(selectors.getAppApiCall)
+
+          console.log({ apiCallSaga: apiCall })
         },
       },
       latest: {
         [sagaActionType]: function*() {
-          yield put(actions.setApiCallData({ sagaWorkedOnLatest: true }))
+          yield put(actions.setAppApiCallData({ sagaWorkedOnLatest: true }))
         },
       },
       [sagaActionType]: function*() {
-        yield put(actions.setApiCallData({ sagaWorked: true }))
+        yield put(actions.setAppApiCallData({ sagaWorked: true }))
       },
     })
 
-    const allSagas = combineSagas(sagas)
-    const sagaMiddleware = createSagaMiddleware()
-    const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+    runSagas(combineSagas(...[combineSagas(...sagas)]))
 
-    sagaMiddleware.run(allSagas)
+    it(`should properly create types`, () => {
+      expect(types).toMatchObject(storeTypes)
+    })
+    it(`should combine reducers`, () => {
+      expect(reducers).toEqual(expect.any(Function))
+    })
+    it(`should generate actions from a set of reducer definitions`, () => {
+      expect(actions).toMatchObject(storeActions)
+    })
 
-    const expectedState = objectUserDefs
+    it(`should generate selectors from an initial state object`, () => {
+      expect(selectors).toMatchObject(storeSelectors)
 
-    expect(store.getState()).toEqual(expectedState)
+      expect(selectors.getAppMegaNum(store.getState())).toEqual(2)
+    })
+    it(`should register actions created with sagas`, () => {
+      expect(sagas.length).toEqual(3)
 
-    store.dispatch(actions.setMegaNum(20))
+      store.dispatch(sagaAction())
 
-    let state = store.getState()
-
-    expect(state[`mega`][`num`]).toBe(20)
-
-    expect(selectors.getMegaNum(state)).toBe(20)
-    expect(selectors.getMega(state)).toEqual({ ...expectedState.mega, num: 20 })
-
-    store.dispatch(actions.resetMega())
-
-    state = store.getState()
-
-    expect(selectors.getMegaNum(state)).toBe(2)
-    expect(selectors.getMega(state)).toEqual({ ...expectedState.mega })
-
-    expect(selectors.getApiCall(state)).toEqual(apiState)
-    store.dispatch(actions.setApiCallLoaded(true))
-
-    state = store.getState()
-
-    expect(selectors.getApiCall(state)).toEqual({ ...apiState, loaded: true })
-
-    expect(sagas.length).toBe(3)
-
-    store.dispatch(createAction(sagaActionType)())
-
-    expect(selectors.getApiCallData(store.getState())).toEqual({ sagaWorked: true, sagaWorkedOnEvery: true, sagaWorkedOnLatest: true })
+      expect(selectors.getAppApiCallData(store.getState())).toEqual({
+        sagaWorked: true,
+        sagaWorkedOnEvery: true,
+        sagaWorkedOnLatest: true,
+      })
+    })
   })
+
+  describe(`internal utils`, () => {
+    it(`valueOr`, () => {
+      expect(utils.valueOr(undefined, 2)).toEqual(2)
+      expect(utils.valueOr(false, 2)).toEqual(false)
+      expect(utils.valueOr(null, `wow`)).toEqual(`wow`)
+    })
+    it(`get`, () => {
+      const testObj = { deeply: { nested: { wow: true } } }
+
+      expect(utils.get(testObj, [`deeply`, `nested`, `wow`], 9)).toEqual(true)
+      expect(() => {
+        try{
+          expect(utils.get(3, null, 9)).toThrowError()
+        } catch(e) {
+          return e
+        }
+      }).toBeTruthy()
+    })
+
+    it(`keyMirror`, () => {
+      expect(utils.keyMirror([1])).toEqual({ "1" : 1 })
+      expect(utils.keyMirror([`wow`, `bro`])).toEqual({ "wow": `wow`, "bro": `bro` })
+    })
+
+    it(`isObject`, () => {
+      expect(utils.isObject({})).toBe(true)
+      expect(utils.isObject(1)).toBe(false)
+      expect(utils.isObject([1])).toBe(false)
+      expect(utils.isObject(false)).toBe(false)
+      expect(utils.isObject(true)).toBe(false)
+      expect(utils.isObject(`wow`)).toBe(false)
+      expect(utils.isObject(Symbol(`wow`))).toBe(false)
+    })
+  })
+
+  describe(`createRdxModule`, () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { setWhoaWow, resetWhoaWow, ...singleModActions } = storeActions
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { getWhoa, getWhoaWow, getApp, ...singleModSelectors } = storeSelectors
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { SET_WHOA_WOW, ...singleModTypes } = storeTypes
+
+    it(`should generate reducers correctly`, () => {
+      const expectedReducers = {
+        lightSwitch: expect.any(Function),
+        todo: expect.any(Function),
+        mega: expect.any(Function),
+        apiCall: expect.any(Function),
+      }
+
+      expect(generateReducersFromDefs(defs)).toMatchObject(
+        expectedReducers,
+      )
+    })
+    it(`should generate types correctly`, () => {
+      expect(module1.types).toMatchObject(singleModTypes)
+    })
+
+    it(`should generate actions correctly`, () => {
+      expect(module1.actions).toMatchObject(singleModActions)
+    })
+
+    it(`should generate selectors correctly`, () => {
+      const moduleKeys = Object.keys(module1.selectors).sort()
+      const expectedKeys = Object.keys(singleModSelectors).sort()
+
+      expect(moduleKeys).toMatchObject(expectedKeys)
+
+      for (const key of moduleKeys) {
+        expect(expectedKeys.includes(key)).toEqual(true)
+      }
+
+      expect(Object.keys(module1.selectors)).toMatchObject(
+        Object.keys(generateSelectors(module1State, `app`)),
+      )
+    })
+
+    it(`should be able to combine with other modules`, () => {
+      expect(modules).toEqual({
+        types: storeTypes,
+        actions: storeActions,
+        reducers: expect.any(Function),
+        selectors: storeSelectors,
+        state: {
+          app: module1State,
+          whoa: module2State,
+        },
+      })
+
+      expect(generateMappers(modules)).toEqual({
+        mapActions: expect.any(Function),
+        mapState: expect.any(Function),
+      })
+
+      const { mapActions, mapState } = generateMappers<typeof modules['actions'], typeof modules['selectors']>(modules)
+
+      expect(mapActions(`setWhoaWow`)).toEqual(expect.any(Function))
+      expect(mapState(`getWhoaWow`)).toEqual(expect.any(Function))
+      expect(mapState(`getWhoa`)(module2State)).toEqual({
+        getWhoa: { wow: `big if true` },
+      })
+    })
+  })
+
+  describe(`redux utils`, () => {
+    it(`should be able to generate reducer, type, action, and selector names from an initial state object`, () => {
+      const expectedNames = {
+        typeName: `SET_ANY_STRING`,
+        actionName: `setAnyString`,
+        selectorName: `getAnyString`,
+        reducerKey: `anyString`,
+      }
+      const actualNames = createNames(`anyString`)
+
+      for (const name of Object.values(actualNames)) {
+        expect(Object.values(expectedNames).includes(name)).toBeTruthy()
+      }
+    })
+    it(`should create actions from a template string with createTypes`, () => {
+      const customTypesObjectKeys = [`WOW`, `COOL_DUDE`, `SWEET`]
+
+      for (const val of customTypesObjectKeys) {
+        expect(Object.keys(customTypes).includes(val)).toBeTruthy()
+      }
+    })
+
+    it(`should accept a curried version of createAction`, () => {
+      const expectedAction = {
+        type: `type`,
+        payload: true,
+        id: `type`,
+      }
+
+      expect(createAction(`type`)(true)).toEqual(expectedAction)
+      expect(createAction(`type`)(true, { meta: { isAwesome: true } })).toEqual({
+        ...expectedAction,
+        meta: { isAwesome: true },
+      })
+      expect(createAction(`type`)(true, { meta: { isAwesome: true } }, `killer`)).toEqual({
+        ...expectedAction,
+        meta: { isAwesome: true },
+        id: `killer`,
+      })
+    })
+  })
+
+  describe(`Redux interop`, () => {
+    const { actions, selectors, store, mapActions, mapState } = createStore({
+      modules,
+      config: {
+        devtools: {
+          enabled: false,
+        },
+        sagas: {
+          enabled: false,
+        },
+      },
+    })
+
+    const mappedActions = mapActions(`setWhoaWow`, `setAppTodo`, `setAppMetadata`, `setAppMetadataIsCool`, `resetAppMega`)(store.dispatch)
+
+    it(`actually works with redux`, () => {
+      expect(mappedActions).toEqual({
+        setWhoaWow: expect.any(Function),
+        setAppTodo: expect.any(Function),
+        setAppMetadata: expect.any(Function),
+        setAppMetadataIsCool: expect.any(Function),
+        resetAppMega: expect.any(Function),
+      })
+
+      mappedActions.setWhoaWow(5)
+      mappedActions.setAppTodo({ todos: [1, 2, 3] })
+      mappedActions.setAppMetadataIsCool(true)
+      mappedActions.setAppMetadata({ isCool: true })
+
+      const mappedState = mapState({
+        whoaWow: `getWhoaWow`,
+        todo: `getAppTodo`,
+        metadata: `getAppMetadata`,
+      })(store.getState())
+
+      expect(mappedState.whoaWow).toEqual(5)
+      expect(mappedState.metadata).toEqual({ isCool: true })
+
+      const expectedState = {
+        app: {
+          ...module1State,
+          metadata: { isCool: true },
+          todo: { todos: [1, 2, 3] } },
+        whoa: {
+          wow: 5,
+        },
+      }
+
+      expect(store.getState()).toEqual(expectedState)
+
+      store.dispatch(actions.setAppMegaNum(20))
+
+      expect(store.getState().app.mega.num).toEqual(20)
+
+      expect(selectors.getAppMegaNum(store.getState())).toEqual(20)
+      expect(selectors.getAppMega(store.getState())).toEqual({
+        ...expectedState.app.mega,
+        num: 20,
+      })
+
+      mappedActions.resetAppMega()
+
+      expect(selectors.getAppMega(store.getState())).toEqual(module1State.mega)
+      expect(selectors.getAppApiCall(store.getState())).toEqual(apiState)
+
+      store.dispatch(actions.setAppApiCallLoaded(true))
+
+      expect(selectors.getAppApiCall(store.getState())).toEqual({ ...apiState, loaded: true })
+    })
+  })
+
 })
