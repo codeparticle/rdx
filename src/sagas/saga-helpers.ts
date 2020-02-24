b172f8acc999a1f5c42f68532847e63772774d3d
Nick Krause <nick@codeparticle.com>
2020-02-24 00:47:00 -0800

    feat(sagas): add check to ensure that sagas are generators

--- src/sagas/saga-helpers.ts
+++ src/sagas/saga-helpers.ts
@@ -1,6 +1,32 @@
 import { all, takeEvery, takeLatest } from "redux-saga/effects"
 import { SagasObject, DefaultSagasObject } from '../types'
 
+//////////////////////
+// functions that make the main 2 below work properly
+function * TestGenerator() {
+  yield undefined
+}
+
+const generateCombineSagaErrorText = notSaga =>
+  `Arguments provided to combineSagas must be generators or functions that return a generator. Received ${notSaga} instead`
+
+const checkAndReturnGenerator = (s) => {
+  if (s instanceof TestGenerator.constructor && typeof s === `function`) {
+    return s()
+  }
+
+  if (
+    typeof s[Symbol.iterator] == `function` &&
+    typeof s[`next`] == `function` &&
+    typeof s[`throw`] == `function`
+  ) {
+    // it's safe to assume the function is a generator function or a shim that behaves like a generator function
+    return s
+  }
+
+  throw new Error(generateCombineSagaErrorText(s))
+}
+
 const generateTakeEverySagas = (takeEverySagas: DefaultSagasObject): Generator[] => Object.entries(takeEverySagas).reduce((acc, [key, runSaga]) => {
   const worker = function* () {
     yield takeEvery(key, runSaga)
@@ -16,6 +42,8 @@ const generateTakeLatestSagas = (takeLatestSagas: DefaultSagasObject): Generator
 
   return acc.concat(worker())
 }, [])
+
+////////////////////
 
 const generateSagas = (sagas: SagasObject): Generator[] => {
   const { every, latest, ...otherSagas } = sagas
@@ -40,21 +68,29 @@ const generateSagas = (sagas: SagasObject): Generator[] => {
 
 const combineSagas = (
   ...sagas: (Generator | (() => Generator))[]
-): (() => Generator) => function*() {
-  try {
-    yield all(
-      [].concat(sagas)
-        .map(s => typeof s === `function` ? s() : s)
-        .filter(s => s.hasOwnProperty(`next`)),
-    )
-  } catch (e) {
-    const errorMessages = [
-      `Error in root saga:`,
-      e,
-      ...(e?.stack ? [`Stack trace: `, e.stack] : []),
-    ]
+): (() => Generator) => {
 
-    console.error(...errorMessages)
+  if (sagas.length === 1 && !Array.isArray(sagas[0])) {
+    // if this is the case, this has probably already been combined. in any case, we don't need to wrap it in all()
+    checkAndReturnGenerator(sagas[0])
+  }
+
+  return function*() {
+    try {
+      yield all(
+        [].concat(Array.isArray(sagas[0]) ? sagas[0]: sagas)
+          .flat()
+          .map(checkAndReturnGenerator),
+      )
+    } catch (e) {
+      const errorMessages = [
+        `Error in root saga:`,
+        e,
+        ...(e?.stack ? [`Stack trace: `, e.stack] : []),
+      ]
+
+      console.error(...errorMessages)
+    }
   }
 }
 
